4e3b95a76c50a9de0907d40491c50a6118e046aa
Kenny Ortmann <kenny.ortmann@asynchrony.com>
2013-05-01 09:07:58 -0500

    Make some ionic specific changes.

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -27,6 +27,9 @@ alias ls="$LS_CMD"
 alias ll="$LS_CMD -al"
 alias la="$LS_CMD -A"
 alias l="$LS_CMD -lahrtc"
+alias gk="gitk 2> /dev/null"
+alias ts="cd workspace/townhall_server"
+alias is="cd workspace/ionic_api_server"
 
 # Syntax highlighting for less (-R for RAW ^ colors)
 alias less='less -R'
@@ -58,6 +61,57 @@ if [ ! $(uname -s) = "Darwin" ]; then
   alias pbcopy='xsel --clipboard --input'
   alias pbpaste='xsel --clipboard --output'
 fi
+
+function get_token()
+{
+  . resty
+  resty http://localhost:3000
+  KEY=2237c2051d9355c0e3981804b62b4e61455ede1564df948e1a556b7690b5a690
+  TOKEN=`resty PUT /access_token.json -d "access_token[username]=joe&access_token[password]=pass1234&access_token[app_api_key]=$KEY" | 
+            jsawk 'return this.access_token.id'`
+  resty http://localhost:3000 -H "IM-AccessToken: $TOKEN"
+  echo "Logged in as $USER"
+}
+
+function get_token_local()
+{
+  . resty
+  resty http://localhost:3000
+  KEY=2237c2051d9355c0e3981804b62b4e61455ede1564df948e1a556b7690b5a690
+  echo "username:"
+  read username
+  TOKEN=`resty PUT /access_token.json -d "access_token[username]=$username&access_token[password]=pass1234&access_token[app_api_key]=$KEY" | 
+            jsawk 'return this.access_token.id'`
+  resty http://localhost:3000 -H "IM-AccessToken: $TOKEN"
+  echo "Logged in as $username"
+}
+
+function get_qa_token()
+{
+  . resty
+  resty http://qa-ionicmobile.asolutions.com
+  KEY=3ac6607abbdb98e1063f4f63a4369278b9ba0de47284a40f097de13faa22aa5c
+  TOKEN=`resty PUT /access_token.json -d "access_token[username]=joe&access_token[password]=pass1234&access_token[app_api_key]=$KEY" | 
+            jsawk 'return this.access_token.id'`
+  resty http://qa-ionicmobile.asolutions.com -H "IM-AccessToken: $TOKEN"
+}
+
+function get_staging_token()
+{
+  . resty
+  resty http://staging-ionicmobile.asolutions.com
+  KEY=9118d6a5f4f3818acc287ec117ee8d767a83b39ce1332efe468ce004bd26e863
+  echo "username:"
+  read username
+  echo "password:"
+  read -s password
+  TOKEN=`resty PUT /access_token.json -d "access_token[username]=$username&access_token[password]=$password&access_token[app_api_key]=$KEY" | 
+            jsawk 'return this.access_token.id'`
+  resty http://staging-ionicmobile.asolutions.com -H "IM-AccessToken: $TOKEN"
+}
+
+alias stop_elastic_search="launchctl unload -wF ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist"
+alias start_elastic_search="launchctl load -wF ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist"
 
 for FILE in ~/dotfiles/*/aliases.sh; do
   source $FILE
