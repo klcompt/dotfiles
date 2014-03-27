b68931db2c3cd3bca736e869f6801b860016b270
jhainline <jhainline@joehainline-ml1.asynchrony.com>
2014-03-27 13:22:46 -0500

    Added a deployment script that shallow copies and zips the output, for sending to customers, or doing local AWS deployment

---
+++ bin/deploy_source_zip.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "This script shallow clones all repos in your directory,"
+    echo "and zips them up for a deployment."
+    echo
+    echo "Usage: please run from your secure_share_workspace directory!"
+    echo "Usage: deploy_source {git branch to deploy} {output zip file}"
+    exit 1
+fi
+DEPLOY_BRANCH=$1
+OUTPUT_FILE=$2
+TEMP_DIR=temp_$(date +%F%T)
+
+echo
+echo This command will create a zip file of latest shallowly cloned source across all repos.
+echo This may take some time...
+read -p "Press [Enter] key to continue"
+echo
+
+DIRS=`ls -l $MYDIR | egrep '^d' | awk '{print $9}'`
+# "ls -l $MYDIR"     = get a directory listing
+# "| egrep '^d'"     = pipe to egrep and select only the directories
+# "awk '{print $9}'" = pipe the result from egrep to awk and print only the 9th field
+# result will be all the repo names
+
+mkdir ${TEMP_DIR}
+cd ${TEMP_DIR}
+
+# and now loop through the repo names:
+for DIR in $DIRS
+do
+  git clone https://git.asynchrony.com/proj-1016/${DIR} --depth 1 -b ${DEPLOY_BRANCH}
+done
+
+echo
+echo Source shallow cloned.  Zipping output...
+echo
+zip -r ../${OUTPUT_FILE} .
+echo
+echo Zip file created.  Cleaning up...
+cd ..
+rm -rf ${TEMP_DIR}
+echo Done!
+echo
+
+
+
